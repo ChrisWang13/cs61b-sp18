4316fa229e7c87975005b0f58e8a61f225d1e78a
ChrisWang13 <chrisqq13@gmail.com>
2022-12-28 09:16:31 +0800

    [lab9] Update operation in BSTMap

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -1,15 +1,16 @@
 package lab9;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
 /**
  * Implementation of interface Map61B with BST as core data structure.
  *
- * @author Your name here
+ * @author Chrisqq13
  */
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
-
+    // Generic keys K have a compareTo method. e.g., key.compareTo().
     private class Node {
         /* (K, V) pair stored in this Node. */
         private K key;
@@ -22,29 +23,28 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         private Node(K k, V v) {
             key = k;
             value = v;
+            left = null;
+            right = null;
         }
     }
 
     private Node root;  /* Root node of the tree. */
     private int size; /* The number of key-value pairs in the tree */
 
-    /* Creates an empty BSTMap. */
+    /**
+     * Creates an empty BSTMap.
+     */
     public BSTMap() {
         this.clear();
     }
 
-    /* Removes all of the mappings from this map. */
+    /**
+     * Removes all the mappings from this map.
+     */
     @Override
     public void clear() {
         root = null;
         size = 0;
-    }
-
-    /** Returns the value mapped to by KEY in the subtree rooted in P.
-     *  or null if this map contains no mapping for the key.
-     */
-    private V getHelper(K key, Node p) {
-        throw new UnsupportedOperationException();
     }
 
     /** Returns the value to which the specified key is mapped, or null if this
@@ -52,36 +52,28 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        return getRecursiveHelper(key, root);
     }
 
-    /** Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
-      * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
-     */
-    private Node putHelper(K key, V value, Node p) {
-        throw new UnsupportedOperationException();
-    }
-
-    /** Inserts the key KEY
-     *  If it is already present, updates value to be VALUE.
-     */
+    /** Inserts <K,V> pair, If it is already present, updates value to be VALUE.*/
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        root = putRecursiveHelper(key, value, root);
+        size = size + 1;
     }
 
-    /* Returns the number of key-value mappings in this map. */
+    /** Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
-    //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
-
-    /* Returns a Set view of the keys contained in this map. */
+    /** Returns a Set view of the keys contained in this map. */
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        HashSet<K> set = new HashSet<>();
+        addKeys(root, set);
+        return set;
     }
 
     /** Removes KEY from the tree if present
@@ -90,20 +82,138 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        if (containsKey(key)) {
+            V res = get(key);
+            root = removeRecursiveHelper(root, key);
+            size = size - 1;
+            return res;
+        }
+        return null;
     }
 
-    /** Removes the key-value entry for the specified key only if it is
-     *  currently mapped to the specified value.  Returns the VALUE removed,
-     *  null on failed removal.
+    /**
+     * Removes the key-value entry for the specified key only if it is
+     * currently mapped to the specified value.  Returns the VALUE removed,
+     * null on failed removal.
      **/
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (containsKey(key)) {
+            V res = get(key);
+            if (res == value) {
+                root = removeRecursiveHelper(root, key);
+                size = size - 1;
+                return res;
+            }
+        }
+        return null;
+    }
+
+    private Node removeRecursiveHelper(Node node, K key) {
+        // 1. No child, 2. Single child, 3. Two child nodes.
+        // a. Left child right most ( < node, > node.left, < node.right)
+        // b. right child left most ( > node, > node.left, < node.right)
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = removeRecursiveHelper(node.left, key);
+        } else if (cmp > 0) {
+            node.right = removeRecursiveHelper(node.right, key);
+        } else {
+            // Find dest node to delete
+            // Case 1 and case 2
+            if (node.left == null) {
+                return node.right;
+            }
+            if (node.right == null) {
+                return node.left;
+            }
+            // Case 3
+            // Solution1 : Replace with <K, V> pair.
+            int i = (int) (Math.random() * 2);
+            if (i == 0) {
+                Node leftRightMostNode = getRightMost(node.left);
+                node.key = leftRightMostNode.key;
+                node.value = leftRightMostNode.value;
+                leftRightMostNode = null;
+            }
+            // Solution2: Replace with leftRightMostNode.
+            else if (i == 1) {
+                Node leftRightMostNode = getRightMost(node.left);
+                // Delete node by returning null to parent or return left or right subtree to parent node.
+                leftRightMostNode.left = removeRecursiveHelper(node.left, leftRightMostNode.key);
+                leftRightMostNode.right = node.right;
+                return leftRightMostNode;
+            }
+        }
+        return node;
+    }
+
+    public V printRightMost() {
+        Node rightMostNode = getRightMost(root);
+        return rightMostNode.value;
+    }
+
+    private Node getRightMost(Node node) {
+        if (node.right != null) {
+            return getRightMost(node.right);
+        }
+        return node;
     }
 
     @Override
     public Iterator<K> iterator() {
-        throw new UnsupportedOperationException();
+        return keySet().iterator();
     }
+
+    /**
+     * Helper function to KeySet to add keys.
+     */
+    private void addKeys(Node node, HashSet set) {
+        // Pre-order transversal
+        if (node == null) {
+            return;
+        }
+        set.add(node.key);
+        addKeys(node.left, set);
+        addKeys(node.right, set);
+    }
+
+    /**
+     * Returns the value mapped to by KEY in the subtree rooted in P.
+     * or null if this map contains no mapping for the key.
+     */
+    private V getRecursiveHelper(K key, Node node) {
+        if (node == null) return null;
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            // Go to left node.
+            return getRecursiveHelper(key, node.left);
+        } else if (cmp > 0) {
+            // Go to right node.
+            return getRecursiveHelper(key, node.right);
+        }
+        return node.value;
+    }
+
+    /**
+     * Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
+     * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
+     */
+    private Node putRecursiveHelper(K key, V value, Node node) {
+        // Naive implementation, future rotation is required.
+        if (node == null) {
+            return new Node(key, value);
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = putRecursiveHelper(key, value, node.left);
+        } else if (cmp > 0) {
+            node.right = putRecursiveHelper(key, value, node.right);
+        }
+        return node;
+    }
+
 }
